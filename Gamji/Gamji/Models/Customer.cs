e6482eb14fb38bdaa047d980c381f7fd4c36545f
rodmjay <rodmjay@wcpro.com>
2014-02-01 14:18:28 -0600

    Create Customer Model

--- Gamji/Gamji/Models/Customer.cs
+++ Gamji/Gamji/Models/Customer.cs
@@ -1,14 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace Gamji.Models
+﻿namespace Gamji.Models
 {
     public class Customer
     {
         public int CustomerId { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string VerificationCode { get; set; }
     }
 }
