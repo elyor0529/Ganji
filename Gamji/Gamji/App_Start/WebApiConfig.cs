8c02c4cf0793843a40435df083e7c61b67949f30
edgar <edgar@ideafortune.com>
2014-02-06 14:06:27 -0600

    GJI-8 - Setup WebApi routes

--- Gamji/Gamji/App_Start/WebApiConfig.cs
+++ Gamji/Gamji/App_Start/WebApiConfig.cs
@@ -12,26 +12,27 @@ namespace Gamji
 			config.Formatters.Remove(config.Formatters.XmlFormatter);
 			config.Formatters.Insert(0, new CamelCaseJsonFormatter());
 
-			config.EnsureInitialized();
-			config.Routes.MapHttpRoute(
-				  name: "ApiById",
-				  routeTemplate: "api/{controller}/{id}",
-				  defaults: new { id = RouteParameter.Optional },
-				  constraints: new { id = @"^[0-9]+$" }
-			  );
-
+			//config.EnsureInitialized();
 			config.Routes.MapHttpRoute(
 				name: "ApiByName",
-				routeTemplate: "api/{controller}/{action}/{name}",
-				defaults: null,
-				constraints: new { name = @"^[a-z]+$" }
+				routeTemplate: "api/{controller}/{action}/{id}",
+				defaults: null //,
+				//constraints: new { name = @"^[a-z]+$" }
 			);
 
-			config.Routes.MapHttpRoute(
-				name: "ApiByAction",
-				routeTemplate: "api/{controller}/{action}",
-				defaults: new { action = "Get" }
-			);
+			//config.Routes.MapHttpRoute(
+			//	name: "ApiById",
+			//	routeTemplate: "api/{controller}/{id}",
+			//	defaults: new { id = RouteParameter.Optional },
+			//	constraints: new { id = @"^[0-9]+$" }
+			//);
+
+
+			//config.Routes.MapHttpRoute(
+			//	name: "ApiByAction",
+			//	routeTemplate: "api/{controller}/{action}",
+			//	defaults: new { action = "Get" }
+			//);
 		}
 	}
 }
